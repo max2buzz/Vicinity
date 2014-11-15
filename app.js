6cd2b7bb263940aec96619631992409ecd119ae7
max2buzz <maxbuzzer92@gmail.com>
2014-11-14 20:36:17 -0600

    asd

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
+var https = require('https');
 var path = require('path');
+var fs = require('fs');
 var app = express();
 var cons = require('consolidate');
 var MongoClient = require('mongodb').MongoClient;
-
 var parted = require('parted');
+// app.use(express.compress());
+// app.use(express.staticCache()); 
+var options = {
+  key: fs.readFileSync('public/keys/mykey.pem'),
+  cert: fs.readFileSync('public/keys/mycert.pem')
+};
 
-
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8007);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
@@ -56,5 +62,11 @@ MongoClient.connect(connectionMongoLab, function(err, db) {
     http.createServer(app).listen(app.get('port'), function() {
         console.log('Express server listening on port ' + app.get('port'));
     });
+
+    https.createServer(options, app).listen(8443 ,function() {
+         console.log('Express server Securly listening on port ');
+    });
+
+
 
 });
